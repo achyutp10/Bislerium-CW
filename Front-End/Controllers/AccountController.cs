94ac492243089c59a4d60b5cece873227fe4dfc7
achyut <achyutp10@gmail.com>
2024-05-05 15:58:01 +0545

    For mayung and all

---
+++ Front-End/Controllers/AccountController.cs
@@ -0,0 +1,36 @@
+﻿using Domain;
+using Domain.Entity;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace Front_End.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterModel1 registerModel)
+        {          
+
+            using (var httpClient = new HttpClient())
+            {
+                // return Json(blog);
+
+                StringContent content = new StringContent(JsonConvert.SerializeObject(registerModel), Encoding.UTF8, "application/json");
+
+                using (var response = await httpClient.PostAsync("https://localhost:7250/api/Account/register", content))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    //blog = JsonConvert.DeserializeObject<Blog>(apiResponse);
+                }
+            }
+            return RedirectToAction("Index", "Blog");
+
+        }
+    }
+}
